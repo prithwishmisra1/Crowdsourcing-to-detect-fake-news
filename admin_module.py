c10a5e91448fc5db67837edfcb5340a16a27cd88
prithwishmisra1 <prithwishmisra1@gmail.com>
2019-04-11 16:07:02 +0530

    second commit

--- admin_module.py
+++ admin_module.py
@@ -8,14 +8,10 @@ from firebase import firebase
 fb = firebase.FirebaseApplication('https://crowdsourcing101-357ca.firebaseio.com/', None)
 
 news_list = fb.get('/News', None)
-
-print(news_list.keys())
-
-print(len(news_list.keys()))
+user_list = fb.get('/Users', None)
 
 subset_list = {}
 for i in news_list.keys():
-    print(i)
     if 'users_flagged' in news_list[i].keys(): #checking if the news has ever been flagged
 
         probability = 0
@@ -25,13 +21,30 @@ for i in news_list.keys():
 
             flag = news_list[i]['users_flagged'][user_id]
             if flag == 1:
-                alpha = fb.get('Users/'+user_id,'alpha')
+                alpha = user_list[user_id]['alpha']
                 probability = probability + alpha
             elif flag == -1:
-                beta = fb.get('Users/'+user_id,'beta')
+                beta = user_list[user_id]['beta']
                 probability = probability - beta
         #calculating utility factor of each news
-        subset_list.update({i:(probability/total_flags)*(news_list[i]['users_seen_infinity']-len(news_list[i]['user_seen']))})
+        if probability < 0:
+            subset_list.update({i:(probability/total_flags)*(news_list[i]['users_seen_infinity']-len(news_list[i]['user_seen']))})
 
 
-print(subset_list)
+if subset_list != None:
+
+    subset_list_keys =  list(subset_list.keys())
+    subset_list_values = list(subset_list.values())
+    #finding the k news for which we will perform manual analysis
+    final_subset_list = {}
+    k = 10
+    while k > 0 or len(subset_list_keys) == len(final_subset_list.keys()):
+
+        value = min(subset_list_values)
+        index = subset_list_values.index(value)
+        key = subset_list_keys[index]
+        final_subset_list.update({key:value})
+        subset_list_values[index] = 1.0
+        k = k - 1
+
+print(final_subset_list)
